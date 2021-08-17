f896de3130cff2f00b6985c68cdb3b22bbc4739a
Medukha1990 <72574597+Medukha1990@users.noreply.github.com>
2021-08-17 13:10:07 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var person = 'anna';
-function palindrome(str) {
-    var reverseStr = str.split('').reverse().join('');
-    if (reverseStr === str) {
-        return true;
-    }
-    return false;
-}
-console.log(palindrome(person));
